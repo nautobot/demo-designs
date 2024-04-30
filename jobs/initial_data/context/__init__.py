e6125984e7e72a789916099a4790a4d790048e17
Christian Adell <chadell@gmail.com>
2024-04-30 09:02:36 +0200

    feat: add two input variables for initial-data design

--- jobs/initial_data/context/__init__.py
+++ jobs/initial_data/context/__init__.py
@@ -3,3 +3,6 @@ from nautobot_design_builder.context import Context
 
 class InitialDesignContext(Context):
     """Render context for basic design"""
+
+    routers_per_site: int
+    custom_description = str
