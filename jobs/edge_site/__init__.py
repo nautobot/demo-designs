f346f81e239edbad859008db25364cdbdf25c287
scetron <stephen.corry@networktocode.com>
2026-03-26 13:18:50 -0400

    edge site to design

--- jobs/edge_site/__init__.py
+++ jobs/edge_site/__init__.py
@@ -28,6 +28,7 @@ class EdgeDesign(DesignJob):
         design_file = "designs/0001_design.yaml.j2"
         context_class = EdgeDesignContext
         nautobot_version = ">=2"
+        design_mode = "Deployment"
 
 
 name = "Demo Designs"
